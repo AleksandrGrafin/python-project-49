d7ee36fae54e2862f5a31f1454fc8fc1808b03f8
AleksandrGrafin <Aleksandr.Sh1991@yandex.ru>
2026-08-24 14:05:35 +0500

    brain-games up

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,6 @@
 import prompt
+
 def welcome_user():
-	print ('Welcome to the Brain Games')
-	name=prompt.string('may I have your name?')
-print (f'Hello,{name})'
+    print ('Welcome to the Brain Games!')
+    name=prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
